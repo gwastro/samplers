1e6e06ec606f7a39fe7e9c3bca0a8df894e2d697
Alexander Harvey Nitz <alex.nitz@gmail.com>
2021-02-07 00:33:54 +0100

    artefact

--- test.sh
+++ test.sh
@@ -3,13 +3,13 @@ echo $SAMPLER
 OMP_NUM_THREADS=1 pycbc_inference \
 --config-file tests/${TEST}.ini sampler_configs/${SAMPLER}.ini \
 --nprocesses=1 \
---output-file ${TEST}-${SAMPLER}.hdf \
+--output-file ${TEST}_${SAMPLER}.hdf \
 --seed 0 \
 --force \
 --verbose
 
 pycbc_validate_test_posterior \
---input-file ${TEST}-${SAMPLER}.hdf \
---output-file ${TEST}-${SAMPLER}.png \
+--input-file ${TEST}_${SAMPLER}.hdf \
+--output-file ${TEST}_${SAMPLER}.png \
 --p-value-threshold .001 \
 --ind-samples 1000
